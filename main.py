60503a4aba8c981346887a37074bbe2fe78e7ffc
Alireza Mohammadi <daban.am56@gmail.com>
2019-02-09 14:26:30 +0330

    parameter tuning

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ from sklearn import preprocessing
 from progress.bar import Bar
 
 gabor_filters = []
-for theta in np.arange(0, np.pi, np.pi / 8):
+for theta in np.arange(0, np.pi, np.pi / 4):
     parameters = GaborParameters(Shape(31, 31), 4.0, theta, 20, 0.5, 0)
     gabor_filters.append(parameters)
 
 
 def random_generator():
-    return random.uniform(0.000, 0.1)
+    return random.uniform(0, 0.1)
 
 
 # time_diff = t_post - t_pre
@@ -49,7 +49,7 @@ second_category = "stop_sign"
 first_category_parent_dir = path.join(source_dir, first_category)
 second_category_parent_dir = path.join(source_dir, second_category)
 
-number_of_image_repeats = 3
+number_of_image_repeats = 1
 
 print("training MNN on {}: ".format(first_category))
 # run on first category
@@ -99,14 +99,11 @@ print("training SVM and compute accuracy by Cross-Validation...")
 # scale data
 data_profile = preprocessing.scale(data_profile)
 
-# # normalize data
-# data_profile = preprocessing.normalize(data_profile, "l2")
-
 # initialize SVM
 clf = SVC(gamma='auto')
 
 # perform Cross-Validation and compute accuracy
-cv = ShuffleSplit(n_splits=10, test_size=0.1, random_state=0)
+cv = ShuffleSplit(n_splits=50, test_size=0.1, random_state=0)
 scores = cross_val_score(clf, data_profile, label_profile, cv=cv)
 
 print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
